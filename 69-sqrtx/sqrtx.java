7bd5b77fcd4199cb4a65b7cdf35bce67222625f2
RIDA FATHIMA <ridafathima2310@gmail.com>
2026-09-11 00:00:52 +0530

    Time: 1 ms (99.03%) | Memory: 42.7 MB (42.78%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int l = 1, r = x / 2;
+        int ans = 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (mid <= x / mid) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
