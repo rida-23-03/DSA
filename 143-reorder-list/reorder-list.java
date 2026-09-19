ecd768281e3bac064a0697709335dbae278e39da
RIDA FATHIMA <ridafathima2310@gmail.com>
2026-09-19 23:39:50 +0530

    Time: 1 ms (99.81%) | Memory: 49 MB (86.67%) - LeetSync

---
+++ 143-reorder-list/reorder-list.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void reorderList(ListNode head) {
+
+        if(head == null || head.next == null)
+            return;
+
+        // 1. Find middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. Reverse second half
+        ListNode prev = null;
+        ListNode curr = slow.next;
+
+        slow.next = null;
+
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // 3. Merge alternately
+        ListNode first = head;
+        ListNode second = prev;
+
+        while(second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
