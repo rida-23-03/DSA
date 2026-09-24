c86b5b83200996879de5260129af96f00677d732
RIDA FATHIMA <ridafathima2310@gmail.com>
2026-09-24 09:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (66.83%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] p) {
+        int profit=0;
+        for(int i=0;i<p.length-1;i++){
+            if(p[i]<p[i+1]){
+                profit+=p[i+1]-p[i];
+            }
+        }
+        return profit;
+    }
+}
